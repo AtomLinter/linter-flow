9c14550b27c7880c6b77e83d53e69906d1176d6e
Sami Kukkonen <sami@samikukkonen.fi>
2016-06-03 12:29:45 +0300

    Report all blames as error messages (#103)
    fix(blames): Report all blames as error messages
    
    * Traces are changed to exclude comments and each message includes traces to
    the other blames in the error.
    
    * flowMessageToLinterMessage -> flowMessageToLinterMessages
    
    * Fix return type of flowMessageToLinterMessages
    
    * Add declarations for lodash.flatten
    
    * Use toBe for comparing primitives

--- spec/linter-spec.js
+++ spec/linter-spec.js
@@ -16,12 +16,12 @@ describe('Flow provider for Linter', () => {
     waitsForPromise(() =>
       atom.workspace.open(constructorPath).then(editor =>
         lint(editor).then(messages => {
-          expect(messages.length).toEqual(1);
-          expect(messages[0].type).toEqual('Warning');
+          expect(messages.length).toBe(1);
+          expect(messages[0].type).toBe('Warning');
           expect(messages[0].text)
-            .toEqual('number This type is incompatible with an implicitly-returned undefined.');
+            .toBe('number This type is incompatible with an implicitly-returned undefined.');
           expect(messages[0].filePath).toMatch(/.+constructor\.js$/);
-          expect(messages[0].trace.length).toEqual(1);
+          expect(messages[0].trace.length).toBe(0);
           expect(messages[0].range).toEqual({
             start: { row: 6, column: 18 },
             end: { row: 6, column: 24 },
@@ -35,14 +35,32 @@ describe('Flow provider for Linter', () => {
     waitsForPromise(() =>
       atom.workspace.open(arrayPath).then(editor =>
         lint(editor).then(messages => {
-          expect(messages.length).toEqual(1);
-          expect(messages[0].type).toEqual('Warning');
-          expect(messages[0].text).toEqual('number This type is incompatible with string');
+          expect(messages.length).toBe(2);
+
+          expect(messages[0].type).toBe('Warning');
+          expect(messages[0].text).toBe('number This type is incompatible with string');
           expect(messages[0].filePath).toMatch(/.+Arrays\.js$/);
-          expect(messages[0].trace.length).toEqual(2);
+          expect(messages[0].trace.length).toBe(1);
+          expect(messages[0].trace[0].range).toEqual({
+            start: { row: 3, column: 16 },
+            end: { row: 3, column: 22 },
+          });
           expect(messages[0].range).toEqual({
             start: { row: 9, column: 4 },
             end: { row: 9, column: 8 },
+          });
+
+          expect(messages[1].type).toBe('Warning');
+          expect(messages[0].text).toBe('number This type is incompatible with string');
+          expect(messages[0].filePath).toMatch(/.+Arrays\.js$/);
+          expect(messages[0].trace.length).toBe(1);
+          expect(messages[1].trace[0].range).toEqual({
+            start: { row: 9, column: 4 },
+            end: { row: 9, column: 8 },
+          });
+          expect(messages[1].range).toEqual({
+            start: { row: 3, column: 16 },
+            end: { row: 3, column: 22 },
           });
         })
       )
